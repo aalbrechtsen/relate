d52cb357aeabaa19456ac98ce571fe0e3a65b7f8
Anders Albrechtsen <andersdetermig@gmail.com>
2020-05-17 11:41:30 +0200

    remove RCinterface.R, methods:::bind_activation not needed for R.2+

--- R/RCinterface.R
+++
@@ -1,9 +0,0 @@
- 
-.onLoad = function(libname, pkgname) {
-  library.dynam("Relate", pkgname)
-  methods:::bind_activation(TRUE)
-}
-
-.onAttach <- function(libname, package) {
-  methods:::bind_activation(FALSE)
-}
